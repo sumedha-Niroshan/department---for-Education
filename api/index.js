dee8b49bac32dbdd9439a55aadaef9811b618865
sumedha-Niroshan <sumedhan199@gmail.com>
2024-01-31 18:19:02 +0530

    Connect to the data base

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
 
-const app  = express();
+const app = express();
 
-app.listen(3000 , ()=>{
-    console.log('server running on port 3000')
-})
+app.listen(3000, () => {
+  console.log("server running on port 3000");
+});
