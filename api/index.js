5f1c6525419a732d92fa725c1d8ce0a1079ea44d
sumedha-Niroshan <sumedhan199@gmail.com>
2024-01-31 19:31:46 +0530

    Connect to the data base

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connect to the mongodb");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("server running on port 3000");
 });
+
+
